5d2ea6c061c86d94a09665f31a3d787d3e5732d9
PranavM3 <pranav.m2024aids@sece.ac.in>
2026-05-04 23:55:18 +0530

    connected frontend to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: "https://event-management-system-5h3m.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
